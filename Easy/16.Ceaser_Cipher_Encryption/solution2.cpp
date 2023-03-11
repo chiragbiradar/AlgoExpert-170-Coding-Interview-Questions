a64984466cee1c3e23931b2b9f91e4ae05c3b2f4
Chirag Biradar <chiragsb16@gmail.com>
2023-03-11 19:54:09 +0530

    16 easy

--- Easy/16.Ceaser_Cipher_Encryption/solution2.cpp
+++ Easy/16.Ceaser_Cipher_Encryption/solution2.cpp
@@ -6,21 +6,22 @@ using namespace std;
 
 // O(n) time and O(n) space complexity
 
-char getNewLetter(char letter, int key)
+char getNewLetter(char letter, int key, string alphabet)
 {
-    int newLetterCode = letter + key;
-    if (newLetterCode <= 122)
-        return newLetterCode;
+    int newLetterCode = alphabet.find(letter)+key;
+    if (newLetterCode <= 25)
+        return alphabet[newLetterCode];
     else
-        return 96 + newLetterCode % 122;
+        return alphabet[-1 + newLetterCode % 25];
 }
 string CeaserCipherEncryption(string s, int key)
 {
     vector<char> newLetters;
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
     int newKey = key % 26;
     for (int i = 0; i < s.size(); i++)
     {
-        newLetters.push_back(getNewLetter(s[i], newKey));
+        newLetters.push_back(getNewLetter(s[i], newKey,alphabet));
     }
     return string(newLetters.begin(), newLetters.end());
 }
